04b1e65a835625572ece16bf2e38ac05ffcb164f
Gnodesign <ginoaliaj@hotmail.com>
2024-04-05 18:18:06 +0300

    term select field improvements

--- templates/form-fields/term-select-field.php
+++ templates/form-fields/term-select-field.php
@@ -8,19 +8,21 @@
  * @author      Automattic
  * @package     wp-job-manager
  * @category    Template
- * @version     1.31.1
+ * @version     $$next-version$$
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+$term = get_term_by( 'slug', $field['default'], $field['taxonomy'] );
+
 // Get selected value.
 if ( isset( $field['value'] ) ) {
 	$selected = $field['value'];
 } elseif ( is_int( $field['default'] ) ) {
 	$selected = $field['default'];
-} elseif ( ! empty( $field['default'] ) && ( $term = get_term_by( 'slug', $field['default'], $field['taxonomy'] ) ) ) {
+} elseif ( ! empty( $field['default'] ) && $term ) {
 	$selected = $term->term_id;
 } else {
 	$selected = '';
@@ -31,14 +33,22 @@ if ( is_array( $selected ) ) {
 	$selected = current( $selected );
 }
 
-wp_dropdown_categories( apply_filters( 'job_manager_term_select_field_wp_dropdown_categories_args', [
+$args = [
 	'taxonomy'         => $field['taxonomy'],
 	'hierarchical'     => 1,
-	'show_option_all'  => false,
+	'show_option_all'  => true,
 	'show_option_none' => $field['required'] ? '' : '-',
 	'name'             => isset( $field['name'] ) ? $field['name'] : $key,
 	'orderby'          => 'name',
 	'selected'         => $selected,
-	'hide_empty'       => false
-], $key, $field ) );
+	'hide_empty'       => false,
+	'multiple'         => false,
+];
+
+if ( isset( $field['placeholder'] ) && ! empty( $field['placeholder'] ) ) {
+	$args['placeholder'] = $field['placeholder'];
+}
+
+job_manager_dropdown_categories( apply_filters( 'job_manager_term_select_field_wp_dropdown_categories_args', $args ) );
+
 if ( ! empty( $field['description'] ) ) : ?><small class="description"><?php echo wp_kses_post( $field['description'] ); ?></small><?php endif; ?>
