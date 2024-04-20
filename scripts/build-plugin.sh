9ba02ad38024d31b53dca4ed94b06c0a07fd8bd3
Mikey Arce <mikeyarce@gmail.com>
2024-04-20 13:49:36 -0700

    Build assets during plugin build

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -9,15 +9,18 @@ rsync -av --progress . build/wp-job-manager --exclude build --exclude node_modul
 # Navigate to build directory
 cd build/wp-job-manager
 
-# Step 3: Run composer install without development dependencies
+# Step 3: Build assets
+npm run build:assets
+
+# Step 4: Run composer install without development dependencies
 composer install --no-dev
 
-# Step 4: Zip the entire contents of the build folder into `wp-job-manager.zip` excluding the files from the exclude.lst file
+# Step 5: Zip the entire contents of the build folder into `wp-job-manager.zip` excluding the files from the exclude.lst file
 # Navigate one level up, so the zip command includes the build directory content
 cd ..
 zip -r wp-job-manager.zip wp-job-manager -x@../scripts/exclude.lst
 
-# Step 5: Remove the contents of the build folder except the new zip file
+# Step 6: Remove the contents of the build folder except the new zip file
 rm -rf wp-job-manager
 
 echo "Build process complete. The wp-job-manager.zip file is ready in the /build folder."
